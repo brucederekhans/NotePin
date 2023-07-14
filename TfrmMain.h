59126f53f57e4d41dff9532c12995766ce5c9546
brucederekhans <brucederekhans@gmail.com>
2023-07-14 09:58:32 +0800

    blank main form

---
+++ TfrmMain.h
@@ -0,0 +1,21 @@
+//---------------------------------------------------------------------------
+
+#ifndef TfrmMainH
+#define TfrmMainH
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.StdCtrls.hpp>
+#include <Vcl.Forms.hpp>
+//---------------------------------------------------------------------------
+class TfrmMain : public TForm
+{
+__published:	// IDE-managed Components
+private:	// User declarations
+public:		// User declarations
+	__fastcall TfrmMain(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TfrmMain *frmMain;
+//---------------------------------------------------------------------------
+#endif
